6f60fb768b681cedaadd2bca54fff4d594378f8a
rvrvrv <ibanez19@sbcglobal.net>
2017-03-06 11:19:08 -0600

    Add error-handling and prevent duplicate search requests

--- js/index.js
+++ js/index.js
@@ -1,43 +1,53 @@
 /*jshint browser: true, esversion: 6*/
 /* global $ */
-$(document).ready(function() {
+$(document).ready(function () {
+	//Global vars
+	var $input = $('#searchBox');
+	var $rslts = $('.results');
+	var txt = '';
+	var lastSearch = '';
 
 	//Retrieve search results from Wikipedia
 	function performSearch(theURL) {
-		$('.results').hide();
-		$.getJSON(theURL, function (data) {
-			//Store results in new arrays
-			var titles = data[1];
-			var infos = data[2];
-			var links = data[3];
+		$rslts.hide();
+		$.getJSON(theURL, (data) => {
+				//Store results in new arrays
+				let titles = data[1];
+				let infos = data[2];
+				let links = data[3];
 
-			//Check for no results
-			if (data[1].length === 0) {
-				$('.results').append('<div class="resultsRowTitle">No results found</div>');
-			} else {
-				for (let i = 0; i < titles.length; i++) {
-					$('.results').append(`<div class="resultsRowTitle"><a href="${links[i]}" target="_blank">${titles[i]}</a></div>`);
-					$('.results').append(`<div class="resultsRowInfo">${infos[i]}</div>`);
+				//Check for no results
+				if (data[1].length === 0) {
+					$rslts.append('<div class="resultsRowTitle">No results found</div>');
+				} else {
+					for (let i = 0; i < titles.length; i++) {
+						$rslts.append(`<div class="resultsRowTitle"><a href="${links[i]}" target="_blank">${titles[i]}</a></div>`);
+						$rslts.append(`<div class="resultsRowInfo">${infos[i]}</div>`);
+					}
 				}
-			}
-			//Display all results smoothly
-			$('.results').append(`Retrieved from: ${theURL}<br>`);
-			$('.results').show();
-		});
+			})
+			.done(() => {
+				//Display all results smoothly
+				$rslts.append(`Retrieved from: ${theURL}<br>`);
+				$rslts.slideDown('slow');
+			})
+			.fail(() => {
+				$rslts.append('<div class="resultsRowTitle">An error has occurred. Please try again later.</div>');
+			});
 	}
 
-	//Widen search box
+	//Widen search box to accommodate text
 	function expandSearchBox() {
-		$('#searchBox').css('box-shadow', '0px 12px 20px 5px rgba(0, 100, 150, .6)');
-		$('#searchBox').css('width', '380px');
+		$input.css('box-shadow', '0px 12px 20px 5px rgba(0, 100, 150, .6)');
+		$input.css('width', '380px');
 		$('#searchBtn').fadeIn('fast');
 	}
 
 	//Upon click, expand the search box
-	$('#searchBox').click(expandSearchBox);
+	$input.click(expandSearchBox);
 
 	//Upon keypress, expand the search box
-	$('#searchBox').keydown(expandSearchBox);
+	$input.keydown(expandSearchBox);
 
 	//Open random entry in new tab
 	$('#randomBtn').click(() => {
@@ -45,18 +55,17 @@ $(document).ready(function() {
 		window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank');
 	});
 
-	//Perform the search
+	//Search button click-handling
 	$('#searchBtn').click(() => {
+		//Trim input field to remove white spaces on each side
+		txt = $input.val().trim();
+		$input.val(txt).trigger('change');
 
-		//Trim to remove white spaces on each side
-		let txt = $('#searchBox').val().trim();
-		$('#searchBox').val(txt).trigger('change');
-
-		//Only perform search if field is filled
-		if (txt !== null && txt !== '') {
-			//Clear results, shorten box, and perform search
-			$('.results').empty();
-			$('#searchBox').css('width', (txt.length * 11.6 + 28) + 'px');
+		//Prevent duplicate/invalid requests
+		if (txt !== lastSearch && txt !== '') {
+			lastSearch = txt;
+			$rslts.empty();
+			$input.css('width', (txt.length * 11.6 + 28) + 'px');
 			let URL = `https://en.wikipedia.org/w/api.php?action=opensearch&limit=5&search=${txt}&format=json&callback=?`;
 			performSearch(URL);
 		}
