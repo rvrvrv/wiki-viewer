9e6c2f19a37d0b41bdeed28a10a3cfc716c213ff
rvrvrv <ibanez19@sbcglobal.net>
2017-03-07 10:20:30 -0600

    Performance and design improvements

--- js/index.js
+++ js/index.js
@@ -2,14 +2,16 @@
 /* global $ */
 $(document).ready(function () {
 	//Global vars
-	var $input = $('#searchBox');
+	var $sBox = $('#searchBox');
 	var $rslts = $('.results');
 	var txt = '';
 	var lastSearch = '';
 
 	//Retrieve search results from Wikipedia
 	function performSearch(theURL) {
-		$rslts.hide();
+		$rslts.empty();
+		$rslts.removeClass('fadeOutUp');
+
 		$.getJSON(theURL, (data) => {
 				//Store results in new arrays
 				let titles = data[1];
@@ -29,45 +31,35 @@ $(document).ready(function () {
 			.done(() => {
 				//Display all results smoothly
 				$rslts.append(`Retrieved from: ${theURL}<br>`);
-				$rslts.slideDown('slow');
+				$rslts.addClass('fadeInDown');
 			})
 			.fail(() => {
 				$rslts.append('<div class="resultsRowTitle">An error has occurred. Please try again later.</div>');
 			});
 	}
 
-	//Widen search box to accommodate text
-	function expandSearchBox() {
-		$input.css('box-shadow', '0px 12px 20px 5px rgba(0, 100, 150, .6)');
-		$input.css('width', '380px');
-		$('#searchBtn').fadeIn('fast');
-	}
-
-	//Upon click, expand the search box
-	$input.click(expandSearchBox);
-
-	//Upon keypress, expand the search box
-	$input.keydown(expandSearchBox);
+	//Upon keypress or mouse click, expand the search box
+	$sBox.keydown(() => $sBox.focus());
+	$sBox.click(() => $sBox.focus());
 
 	//Open random entry in new tab
-	$('#randomBtn').click(() => {
-		this.blur();
-		window.open('https://en.wikipedia.org/wiki/Special:Random', '_blank');
-	});
+	$('#randomBtn').click(() => window.open('https://en.wikipedia.org/wiki/Special:Random'));
 
 	//Search button click-handling
 	$('#searchBtn').click(() => {
+		$sBox.blur();
 		//Trim input field to remove white spaces on each side
-		txt = $input.val().trim();
-		$input.val(txt).trigger('change');
+		txt = $sBox.val().trim();
+		$sBox.val(txt).trigger('change');
 
-		//Prevent duplicate/invalid requests
+		//If request is not a duplicate or blank, perform the search
 		if (txt !== lastSearch && txt !== '') {
 			lastSearch = txt;
-			$rslts.empty();
-			$input.css('width', (txt.length * 11.6 + 28) + 'px');
 			let URL = `https://en.wikipedia.org/w/api.php?action=opensearch&limit=5&search=${txt}&format=json&callback=?`;
-			performSearch(URL);
+			$sBox.css('width', (txt.length * 11.5 + 27) + 'px');
+			$rslts.removeClass('fadeInDown');
+			$rslts.addClass('fadeOutUp');
+			setTimeout(() => performSearch(URL), 500);
 		}
 	});
 
