ef1367ba7d4de21c98ca9d4ed9752c92018d5f53
rvrvrv <ibanez19@sbcglobal.net>
2017-12-24 09:22:45 -0600

    Refactor generated HTML

--- js/index.js
+++ js/index.js
@@ -7,11 +7,10 @@ $(document).ready(() => {
   let lastSearch = '';
 
   // Retrieve search results from Wikipedia
-  function performSearch(theURL) {
+  function performSearch(query) {
     $rslts.empty();
-    $rslts.removeClass('fadeOutUp');
-
-    $.getJSON(theURL, (data) => {
+    const url = `https://en.wikipedia.org/w/api.php?action=opensearch&limit=5&search=${query}&format=json&callback=?`;
+    $.getJSON(url, (data) => {
       // Store results in new arrays
       const titles = data[1];
       const infos = data[2];
@@ -19,26 +18,30 @@ $(document).ready(() => {
 
       // Check for no results
       if (data[1].length === 0) {
-        $rslts.append('<div class="resultsRowTitle">No results found</div>');
+        $rslts.append('<div class="entry"><div class="title">No results found</div></div>');
       } else {
-        for (let i = 0; i < titles.length; i++) {
-          $rslts.append(`<div class="resultsRowTitle"><a href="${links[i]}" target="_blank">${titles[i]}</a></div>`);
-          $rslts.append(`<div class="resultsRowInfo">${infos[i]}</div>`);
-        }
+        titles.forEach((t, i) => {
+          $rslts.append(`
+            <div class="entry">
+              <div class="title"><a href="${links[i]}" target="_blank">${titles[i]}</a></div>
+              <div class="info">${infos[i]}</div>
+            </div>
+            `);
+        });
       }
     })
       .done(() => {
         // Display all results smoothly
-        $rslts.append(`Retrieved from: ${theURL}<br>`);
-        $rslts.addClass('fadeInDown');
+        $rslts.append(`Retrieved from: ${url}<br>`);
+        $rslts.removeClass('fadeOutUp').addClass('fadeInDown');
       })
       .fail(() => {
-        $rslts.append('<div class="resultsRowTitle">An error has occurred. Please try again later.</div>');
+        $rslts.append('<div class="entry"><div class="title">No results found</div></div>');
       });
   }
 
   // Upon keypress or mouse click, expand the search box
-  $sBox.keydown(() => $sBox.focus());
+  $sBox.keyup(() => $sBox.focus());
   $sBox.click(() => $sBox.focus());
 
   // Open random entry in new tab
@@ -52,13 +55,11 @@ $(document).ready(() => {
     $sBox.val(txt).trigger('change');
 
     // If request is not a duplicate or blank, perform the search
-    if (txt !== lastSearch && txt !== '') {
-      lastSearch = txt;
-      const URL = `https://en.wikipedia.org/w/api.php?action=opensearch&limit=5&search=${txt}&format=json&callback=?`;
-      $sBox.css('width', `${(txt.length * 11.5) + 27}px`);
-      $rslts.removeClass('fadeInDown');
-      $rslts.addClass('fadeOutUp');
-      setTimeout(() => performSearch(URL), 500);
+    if (txt.toLowerCase() !== lastSearch.toLowerCase() && txt !== '') {
+      lastSearch = txt.toLowerCase();
+      $sBox.css('width', `${(txt.length + 1) * 12}px`);
+      $rslts.removeClass('fadeInDown').addClass('fadeOutUp');
+      setTimeout(() => performSearch(txt), 500);
     }
   });
 });
