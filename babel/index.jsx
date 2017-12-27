cfaeaa6b572f66dcbc71fc217bea7d6bf0954551
rvrvrv <ibanez19@sbcglobal.net>
2017-12-27 08:48:59 -0600

    Improve search responsiveness and implement Babel

---
+++ babel/index.jsx
@@ -0,0 +1,79 @@
+/* global $ */
+$(document).ready(() => {
+  // Global vars
+  const $sBox = $('#searchBox');
+  const $sBtn = $('#searchBtn');
+  const $sIcn = $('#searchIcon');
+  const $rslts = $('.results');
+  let lastSearch = '';
+
+  // Toggle search button and box
+  function toggleSearch(enable) {
+    // Restore search button and box to original state
+    if (enable) {
+      $sIcn.removeClass('fa-circle-o-notch fa-spin').addClass('fa-search');
+      $sBtn.removeAttr('disabled');
+      $sBox.removeAttr('disabled');
+    } else {
+      // Change search icon to animated spinner, and disable button and box
+      $sIcn.removeClass('fa-search').addClass('fa-circle-o-notch fa-spin');
+      $sBtn.attr('disabled', 'disabled');
+      $sBox.attr('disabled', 'disabled');
+    }
+  }
+
+  // Retrieve search results from Wikipedia
+  function performSearch(query) {
+    $rslts.empty();
+    const url = `https://en.wikipedia.org/w/api.php?action=opensearch&limit=5&search=${query}&format=json&callback=?`;
+    $.getJSON(url, (data) => {
+      // Store results in new arrays
+      const titles = data[1];
+      const infos = data[2];
+      const links = data[3];
+      // Check for no results
+      if (!data[1].length) $rslts.append('<div class="entry"><div class="title">No results found.</div></div>');
+      else {
+        // Generate HTML for results
+        titles.forEach((t, i) => {
+          $rslts.append(`
+            <div class="entry">
+              <div class="title"><a href="${links[i]}" target="_blank">${titles[i]}</a></div>
+              <div class="info">${infos[i]}</div>
+            </div>
+            `);
+        });
+      }
+    })
+      .done(() => {
+        // Display all results smoothly
+        $rslts.append(`Retrieved from: ${url}<br>`);
+        $rslts.removeClass('fadeOutUp').addClass('fadeInDown');
+        toggleSearch(true);
+      })
+      .fail(() => {
+        // If applicable, display error message and re-enable search button
+        $rslts.append('<div class="entry"><div class="title">An error has occurred. Please try again later.</div></div>');
+        toggleSearch(true);
+      });
+  }
+
+  // Search button click-handling
+  $sBtn.mousedown(() => $sBtn.click()); // Handle mousedown events as clicks
+  $sBtn.click(() => {
+    // First, update UI
+    $sBox.blur();
+    toggleSearch();
+    // Trim input field to remove white spaces on each side
+    const txt = $sBox.val().trim();
+    $sBox.val(txt).trigger('change');
+
+    // If request is not a duplicate or blank, perform the search
+    if (txt.toLowerCase() !== lastSearch.toLowerCase() && txt !== '') {
+      lastSearch = txt.toLowerCase();
+      $sBox.css('width', `${(txt.length + 1) * 12}px`);
+      $rslts.removeClass('fadeInDown').addClass('fadeOutUp');
+      setTimeout(() => performSearch(txt), 500);
+    } else toggleSearch(true);
+  });
+});
